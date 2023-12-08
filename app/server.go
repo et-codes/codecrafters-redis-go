6b096e10d1cdadf93d56cb669e17c6d6df1588b3
et-codes <ericthornton43@gmail.com>
2023-12-08 02:21:02 +0000

    pass 1st stage

--- app/server.go
+++ app/server.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"net"
+	"os"
+)
+
+const (
+	redisHost = "localhost"
+	redisPort = 6379
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", redisHost, redisPort))
+	if err != nil {
+		fmt.Printf("Failed to bind to port %d\n", redisPort)
+		os.Exit(1)
+	}
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 }
