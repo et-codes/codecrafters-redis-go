9aa77b75550fd34ebf3f55e117ee4701bf4853b7
et-codes <ericthornton43@gmail.com>
2023-12-10 22:43:38 +0000

    Receive command-line args

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"github.com/et-codes/codecrafters-redis-go/logging"
 )
@@ -11,12 +12,25 @@ var logger = logging.New(logging.LevelDebug)
 func main() {
 	ctx := context.Background()
 
-	// Initiate server.
-	c := ServerConfig{
+	cfg := ServerConfig{
 		Host: "localhost",
 		Port: 6379,
 	}
-	s := NewServer(ctx, c)
+
+	// Get command-line arguments.
+	args := os.Args
+	for i := 1; i < len(args); i += 2 {
+		if args[i] == "--dir" {
+			cfg.DBDir = args[i+1]
+		} else if args[i] == "--dbfilename" {
+			cfg.DBFilename = args[i+1]
+		}
+	}
+
+	logger.Debug("Server config: %+v", cfg)
+
+	// Initiate server.
+	s := NewServer(ctx, cfg)
 	if err := s.Run(); err != nil {
 		logger.Fatal("Server error: ", err)
 	}
