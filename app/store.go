eb709c396a1039dce17ff5e2bfdff9e985bf55a9
et-codes <ericthornton43@gmail.com>
2023-12-11 18:40:40 +0000

    Updates to Store

--- app/store.go
+++ app/store.go
@@ -1,36 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
-type Store map[string]string
-
-func NewStore() *Store {
-	s := make(Store)
-	return &s
+type Store struct {
+	kvMap map[string]string
+	db    io.ReadWriteCloser
 }
 
+func NewStore(db io.ReadWriteCloser) *Store {
+	kv := make(map[string]string)
+	return &Store{kvMap: kv, db: db}
+}
+
+// Load loads the in-memory KV map with values from the db.
+func (s *Store) Load() error {
+	return nil
+}
+
+// Save stores the in-memory KV map to the db.
+func (s *Store) Save() error {
+	return nil
+}
+
+// Get retreives the value for the given key from the KV map. An error
+// is returned if the key is not found.
 func (s *Store) Get(key string) (string, error) {
-	val, found := (*s)[key]
+	val, found := s.kvMap[key]
 	if !found {
 		return "", fmt.Errorf("key %q not found", key)
 	}
 	return val, nil
 }
 
+// Add stores the KV-pair in the KV map. An error will be returned if the
+// key already exists.
 func (s *Store) Add(key, val string) error {
-	_, found := (*s)[key]
+	_, found := s.kvMap[key]
 	if found {
 		return fmt.Errorf("key %q already exists", key)
 	}
-	(*s)[key] = val
+	s.kvMap[key] = val
 	return nil
 }
 
-func (s *Store) Delete(key string) error {
-	_, found := (*s)[key]
+// Update replaces the value of an existing key to a new one. An error is
+// returned if the key is not found.
+func (s *Store) Update(key, val string) error {
+	_, found := s.kvMap[key]
 	if !found {
 		return fmt.Errorf("key %q not found", key)
 	}
-	delete(*s, key)
+	s.kvMap[key] = val
+	return nil
+}
+
+// Delete removes the given key and its value from the KV map. An error
+// is returned if the key is not found.
+func (s *Store) Delete(key string) error {
+	_, found := s.kvMap[key]
+	if !found {
+		return fmt.Errorf("key %q not found", key)
+	}
+	delete(s.kvMap, key)
 	return nil
 }
--- app/store_test.go
+++ app/store_test.go
@@ -7,11 +7,19 @@ import (
 )
 
 func TestStore(t *testing.T) {
-	s := NewStore()
+	s := NewStore(nil)
 
 	t.Run("can add a value", func(t *testing.T) {
 		err := s.Add("hello", "world")
 		assert.NoError(t, err)
+	})
+
+	t.Run("error when adding existing key", func(t *testing.T) {
+		err := s.Add("exists", "value")
+		assert.NoError(t, err)
+
+		err = s.Add("exists", "new value")
+		assert.Error(t, err)
 	})
 
 	t.Run("can get a value", func(t *testing.T) {
@@ -23,6 +31,18 @@ func TestStore(t *testing.T) {
 	t.Run("error when getting non-existent value", func(t *testing.T) {
 		_, err := s.Get("world")
 		assert.Error(t, err)
+	})
+
+	t.Run("can update a value", func(t *testing.T) {
+		err := s.Add("mykey", "my value")
+		assert.NoError(t, err)
+
+		err = s.Update("mykey", "new value")
+		assert.NoError(t, err)
+
+		val, err := s.Get("mykey")
+		assert.NoError(t, err)
+		assert.Equal(t, "new value", val)
 	})
 
 	t.Run("can delete a value", func(t *testing.T) {
